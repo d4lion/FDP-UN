75a1431d87858b88154a8bb00ef79fa51a1563c3
daliondev <d4lion.dev@gmail.com>
2024-02-28 00:34:32 -0500

    :tada: Solved: Rayuela juego

---
+++ minas_lab/practica-3/h-practica-8.py
@@ -0,0 +1,8 @@
+andrea: int = int(input(""))
+sandra: int = int(input(""))
+milena: int = int(input(""))
+
+promedio_andrea_sandra = sum((andrea, sandra)) // 2
+promedio_andrea_milena = sum((andrea, milena)) // 2
+promedio_milena_sandra = sum((milena, sandra)) // 2
+
