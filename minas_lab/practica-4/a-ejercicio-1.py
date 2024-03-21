fa6847d8816f6931df2a424c782d6ac21d5be273
daliondev <d4lion.dev@gmail.com>
2024-03-20 21:51:06 -0500

    :white_check_mark: Solved: Repetir mensaje

---
+++ minas_lab/practica-4/a-ejercicio-1.py
@@ -0,0 +1,12 @@
+"""
+Repetir un mensaje dado la cantidad de veces que diga el usuario
+
+drive = https://drive.google.com/file/d/1JcGDZSCLdyEVHFJDe3RD9QVT00BiZgUE/view
+"""
+
+repeticiones = int(input())
+
+while repeticiones > 0:
+    print("Hola mundo")
+    print("Estoy tomando contacto con el lenguaje de programacion")
+    repeticiones -= 1
