83cbd4b1c31393bf0135b8b543e7a669da8d2e67
d4lion <d4lion.dev@gmail.com>
2024-09-18 22:27:13 -0500

    :sparkles: Solved challenges 6 of practice 8

--- minas_lab/Python/practica-8/f-practica-6.py
+++ minas_lab/Python/practica-8/f-practica-6.py
@@ -13,22 +13,22 @@ print(ls, file=open(f'{path_actual}/utils/ls.txt', 'w'))
 ls_clean = list(map(lambda x: x.replace('\n', '').strip().split(' '), ls))
 
 
-for palabras in ls_clean:
+for traduccion_de_palabras in ls_clean:
     ultima_terminacion = ''
     cumplen = 0
 
-    for index, palabra in enumerate(palabras):
+    for index, palabra in enumerate(traduccion_de_palabras):
         try:
             # Caso 1: Tomando las 2 ultimas letras de la palabra
-            if palabra[-2:] == palabras[index + 1][:2]:
+            if palabra[-2:] == traduccion_de_palabras[index + 1][:2]:
                 cumplen += 1
             # Caso 2: Tomando las 3 ultimas letras de la palabra
-            elif palabra[-3:] == palabras[index + 1][:3]:
+            elif palabra[-3:] == traduccion_de_palabras[index + 1][:3]:
                 cumplen += 1
         except IndexError:
             pass
 
-    if cumplen == len(palabras) - 1:
+    if cumplen == len(traduccion_de_palabras) - 1:
         print('Cadena completa')
     else:
         print('Cadena rota')
